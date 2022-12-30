b1077d09140e40b2a5e808e5b1142a0f8450f1cd
Chandan Pratap <92000681+Chandan8585@users.noreply.github.com>
2022-12-30 23:12:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,144 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menuBtn.onclick = () =>{
+   menuBtn.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".course-slider", {
+   spaceBetween: 20,
+   grabCursor:true,
+   loop:true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   breakpoints: {
+      540: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+var swiper = new Swiper(".teachers-slider", {
+   spaceBetween: 20,
+   grabCursor:true,
+   loop:true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   breakpoints: {
+      540: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+   spaceBetween: 20,
+   grabCursor:true,
+   loop:true,
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+   breakpoints: {
+      540: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+
+// $(function() {
+//   var currentIndex = 0;
+//   var images = $('courses img');
+//   var imageCount = images.length;
+
+//   function updateSlideshow() {
+//     images.eq(currentIndex).fadeOut(function() {
+//       currentIndex++;
+//       if (currentIndex == imageCount) {
+//         currentIndex = 0;
+//       }
+//       images.eq(currentIndex).fadeIn();
+//     });
+//   }
+
+//   setInterval(updateSlideshow, 3000);
+// });
+
+$(function() {
+  var currentIndex = 0;
+  var images = $('#slideshow img');
+  var imageCount = images.length;
+
+  function updateSlideshow() {
+    images.eq(currentIndex).fadeOut(function() {
+      currentIndex++;
+      if (currentIndex == imageCount) {
+        currentIndex = 0;
+      }
+      images.eq(currentIndex).fadeIn();
+    });
+  }
+
+  setInterval(updateSlideshow, 3000);
+});
+
+
+ 
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
